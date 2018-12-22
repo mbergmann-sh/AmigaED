49d4e23f029c4bbe1ba5cab5f5bc15dcd7f2d240
mbergmann-sh <mb@mbergmann-sh.de>
2018-12-22 07:42:35 +0100

    added Linux quick install script

--- install_linux.sh
+++ install_linux.sh
@@ -5,4 +5,7 @@ echo "Hard linking AmigaED as /usr/local/bin/amigaed"
 sudo cp -l -f AmigaED /usr/local/bin/amigaed
 echo "Done."
 echo
+echo "AmigaED can be invoked by 'amigaed <filename>' now"
+echo
+
 
